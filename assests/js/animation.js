76d3185c0bf70a3f675837a3ca54b632adddca8c
SANTHOSHKUMARWEBTHINKER <ss4856026@gmail.com>
2025-10-28 21:39:47 +0530

    animation-removed-santhosh

--- assests/js/animation.js
+++
@@ -1,9 +0,0 @@
-
-anime({
-    targets: '.swiper-animations',
-    opacity: [0, 1],
-    translateY: [40, 0],
-    delay: anime.stagger(300),
-    duration: 1500,
-    easing: 'spring(1, 80, 10, 0)',
-}); 
